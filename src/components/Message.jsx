c4e07ac3c4b900cbec374e4ee8193c3a14da766e
Umar-Farooq-5757 <aamirgel17@gmail.com>
2025-08-31 22:00:42 +0500

    UI fixes

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,10 @@ import React, { useEffect } from "react";
 import moment from "moment";
 import Markdown from "react-markdown";
 import Prism from 'prismjs'
+import { useAppContext } from "../context/AppContext";
 
 const Message = ({ message }) => {
+  const {isDark} = useAppContext()
   useEffect(() => {
   Prism.highlightAll()
   
@@ -12,7 +14,7 @@ const Message = ({ message }) => {
   return (
     <>
       {message.role == "user" ? (
-        <div className="bg-[#f8fafc] ml-32 border border-gray-400 py-1 px-2 text-sm self-end rounded-sm">
+        <div className={` ${isDark?'bg-slate-800':'bg-[#f8fafc]'} ml-32 border border-gray-400 py-1 px-2 text-sm self-end rounded-sm`}>
           <p className="mb-1">{message.content}</p>
           <span className="text-xs  text-gray-500">
             {moment(message.timestamp).fromNow()}
@@ -25,8 +27,8 @@ const Message = ({ message }) => {
           alt=""
         />
       ) : (
-        <div className="self-start mr-24 bg-[#fdede5] p-2 border reset-tw border-[#ec4e02] rounded-md text-sm">
-          <p className="mb-1"><Markdown>{message.content}</Markdown></p>
+        <div className={`self-start mr-24  ${isDark?'bg-[#251812]':'bg-[#fdede5]'} p-2 border reset-tw border-[#ec4e02] rounded-md text-sm`}>
+          <div className="mb-1"><Markdown>{message.content}</Markdown></div>
            <span className="text-xs  text-gray-500">
             {moment(message.timestamp).fromNow()}
           </span>
